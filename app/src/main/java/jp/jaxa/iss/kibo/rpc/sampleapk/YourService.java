1080dd550e7f422fe9ed5f1e93bc49f66b3e19c8
Subhang Mall <subhangmall10@gmail.com>
2025-06-19 00:03:05 -0700

    added waiting for success for movement

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,5 +1,6 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
+import gov.nasa.arc.astrobee.Result;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 import gov.nasa.arc.astrobee.types.Point;
@@ -225,27 +226,55 @@ public class YourService extends KiboRpcService {
         /* If there is a treasure item, remember it.                                        */
         /* ******************************************************************************** */
        // Row 1
-        Point point = new Point(10.95d, -10.58, 5.195d);
-        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point, quaternion, false);
+        Point point1 = new Point(10.95d, -10.58, 5.195d);
+        Quaternion quaternion1 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        Result res1 = api.moveTo(point1, quaternion1, false);
+        while(!res1.hasSucceeded()){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } 
         recognizeImage(1);
 
         // Row 2
-        point = new Point(10.925d, 0.375d, 3.76203d);
-        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point, quaternion, false);
+        Point point2 = new Point(10.925d, 0.375d, 3.76203d);
+        Quaternion quaternion2 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        Result res2 = api.moveTo(point2, quaternion2, false);
+        while(!res2.hasSucceeded()){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } 
         recognizeImage(2);
 
         // Row 3
-        point = new Point(10.925d, -7.925d, 3.76093d);
-        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point, quaternion, false);
+        Point point3 = new Point(10.925d, -7.925d, 3.76093d);
+        Quaternion quaternion3 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        Result res3 = api.moveTo(point3, quaternion3, false);
+        while(!res3.hasSucceeded()){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } 
         recognizeImage(3);
 
         // Row 4
-        point = new Point(9.866984d, -6.8525d, 4.945d);
-        quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
-        api.moveTo(point, quaternion, false);
+        Point point4 = new Point(9.866984d, -6.8525d, 4.945d);
+        Quaternion quaternion4 = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        Result res4 = api.moveTo(point4, quaternion4, false);
+        while(!res4.hasSucceeded()){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         recognizeImage(4);
 
         /* **************************************************** */
